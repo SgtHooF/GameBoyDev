f746e96fe92160a2f07904b387e897c842075844
SgtHooF <sgt_hoof@live.co.uk>
2015-03-19 01:41:50 +0000

    Input functionality
    Game can now respond to input, the player can move and cannot go out of
    bounds also.

--- gbdk/examples/gb/dscan/dscan.c
+++ gbdk/examples/gb/dscan/dscan.c
@@ -4,6 +4,11 @@
 /*|Scroll right down to the bottom for Main().           |*/
 /*+------------------------------------------------------+*/
 
+/*Good Chars for Later:
+		1->4 = the arrow keys
+		5 = box
+*/
+
 /*includes*/
 #include <gb/gb.h>
 #include <stdlib.h>
@@ -20,12 +25,25 @@
 #include "fore.h"
 #include "fore.c"
 
+/*things for the player*/
+char player_icon;
+UINT8 player_oldX;
+UINT8 player_oldY;
+UINT8 player_newX;
+UINT8 player_newY;
+
+/*things for the game*/
+int key;
+
 /*-----MESSAGE-SYSTEM-----*/
 char message[15];
 
 void writemessage()
 {
-	gotoxy(1, 16);
+	gotoxy(0,16);
+	printf(">~-~-~-~-~-~-~-~-~<");
+
+	gotoxy(2, 16);
 	printf("%s\n", message);
 }
 
@@ -60,15 +78,113 @@ void setupscreen()
 	printf("+-----------------+");
 }
 
+/*draws player at new location, and floor at the old*/
+void drawplayer()
+{
+	gotoxy(player_oldX, player_oldY);
+	printf(".");
+	gotoxy(player_newX, player_newY);
+	printf("%c", player_icon);
+}
+
+/*updates the players position based on joypad*/
+void updateplayer()
+{
+	key=joypad();
+	
+	player_oldX = player_newX;
+	player_oldY = player_newY;
+	
+	if(key & J_LEFT)
+	{
+		if(player_oldX > 1)
+		{
+			player_newX=(player_oldX-1);
+		}
+	}
+	
+	if(key & J_RIGHT)
+	{
+		if(player_oldX < 17)
+		{
+			player_newX=(player_oldX+1);
+		}
+	}
+	
+	if(key & J_UP)
+	{
+		if(player_oldY > 1)
+		{
+			player_newY=(player_oldY-1);
+		}
+	}
+	
+	if(key & J_DOWN)
+	{
+		if(player_oldY < 14)
+		{
+			player_newY=(player_oldY+1);
+		}
+	}
+	
+	/*Additional test input logic*/
+	if(key & J_A)
+	{
+		strcpy(message, "Pressed A");
+		writemessage();
+	}
+	
+	if(key & J_B)
+	{
+		strcpy(message, "Pressed B");
+		writemessage();
+	}
+	
+	if(key & J_START)
+	{
+		strcpy(message, "Pressed START");
+		writemessage();
+	}
+	
+	if(key & J_SELECT)
+	{
+		strcpy(message, "Pressed SELECT");
+		writemessage();
+	}
+}
+
 /*----------------------MAIN-------------------------*/
 /*---------------------PROGRAM-----------------------*/
 void main()
 {
+	/*initialize player attributes*/
+	strcpy(message, "Init Player");
+	writemessage();
+	player_icon = 'P';
+	player_oldX = 5;
+	player_oldY = 5;
+	player_newX = 5;
+	player_newY = 5;
+	
+	/*initialize world*/
+	strcpy(message, "Init World");
+	writemessage();
 	setupscreen();
-	strcpy(message, "Hello World");
+	drawplayer();
+	
+	strcpy(message, "Press START");
 	writemessage();
+	
 	waitpad(J_START);
-	strcpy(message, "sfgsdfg");
+	
+	strcpy(message, "Godspeed");
 	writemessage();
-	//printf("NIGGERS");
+	
+	/*Begin game loop*/
+	while(!0)
+	{
+		updateplayer();
+		drawplayer();
+		delay(120);
+	}
 }
